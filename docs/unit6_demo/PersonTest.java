3cf1b3a0dc5dc0c097089d14d39c6f7f692dfdea
Darren Kessner (linux laptop) <darren.kessner@marlborough.org>
2024-10-29 13:38:41 -0700

    updated website

---
+++ docs/unit6_demo/PersonTest.java
@@ -0,0 +1,15 @@
+//
+// PersonTest.java
+//
+
+
+public class PersonTest
+{
+    public static void main(String[] args)
+    {
+        Person person = new Person();
+        person.greeting(7);
+    }
+}
+
+
